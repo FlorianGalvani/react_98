e58406706cdce58d78de4bdcc63cf2a831d776ef
Florian Galvani <florian.galvani@gmail.com>
2022-06-26 13:02:12 +0200

    ✨ add styled-components to start button

--- src/components/Buttons/Start/index.tsx
+++ src/components/Buttons/Start/index.tsx
@@ -1,15 +1,44 @@
-import "./style.css";
+import styled from "styled-components";
 
-const Start: React.FC = () => {
+const StartButton: React.FC = () => {
   const openMenu = () => {
     console.log("Hello");
   };
   return (
-    <button className="start_button" onClick={openMenu}>
+    <Button onClick={openMenu}>
       <img src="/w98_Startup_logo.png" width={16} height={16}></img>
       <p>Démarrer</p>
-    </button>
+    </Button>
   );
 };
 
-export default Start;
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top: 1px solid #fff;
+  border-right: 1px solid #575757;
+  border-bottom: 1px solid #575757;
+  border-left: 1px solid #fff;
+  box-shadow: 1px 1px 0 #000;
+  padding: 6px 4px;
+  background-color: #818181;
+  &:focus {
+    box-shadow: inset 1px 1px 0 #000;
+    border-top: 1px solid #575757;
+    border-right: 1px solid #fff;
+    border-bottom: 1px solid #fff;
+    border-left: 1px solid #575757;
+  }
+  img {
+    user-select: none;
+  }
+  p {
+    margin-left: 4px;
+    font-size: 15px;
+    user-select: none;
+    font-family: "Win98";
+  }
+`;
+
+export default StartButton;
--- src/components/Taskbar/index.tsx
+++ src/components/Taskbar/index.tsx
@@ -1,9 +1,9 @@
-import Start from "../Buttons/Start";
+import StartButton from "../Buttons/Start";
 import "./style.css";
 const Taskbar: React.FC = () => {
   return (
     <div className="taskbar">
-      <Start />
+      <StartButton />
     </div>
   );
 };
