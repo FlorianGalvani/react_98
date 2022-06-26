49c8cd2dc4514b17aaa10dcf7ceeb0b210cac3cd
Florian Galvani <florian.galvani@gmail.com>
2022-06-26 15:24:53 +0200

    💄 create start menu list separator component

---
+++ src/components/Menus/Start/List/Separator/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Separator: React.FC = () => {
+  return <Container></Container>;
+};
+
+const Container = styled.div`
+  height: 1px;
+  background-color: #2a2a2a;
+  border-bottom: 1px solid #fff;
+`;
+
+export default Separator;
